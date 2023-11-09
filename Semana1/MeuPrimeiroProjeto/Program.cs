29f5a6304bbb6f4fd0e3ed095bd7fb5608b9275c
Alan Carlos <alancarlospereira.ti@gmail.com>
2023-11-09 16:51:58 -0300

    Commit inicial

---
+++ Semana1/MeuPrimeiroProjeto/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+#region Teste de tipos de Dados
+    int tipoInteiro;
+    double tipoDouble;
+    string tipoString;
+    long tipoLong;
+
+    tipoInteiro = int.MaxValue;
+    tipoLong = long.MaxValue;
+
+    Console.WriteLine("O maximo inteiro é " + tipoInteiro);
+    Console.WriteLine("O maximo inteiro é " + tipoLong);
+    
+#endregion
