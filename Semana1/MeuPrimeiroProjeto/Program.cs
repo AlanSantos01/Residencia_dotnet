e44fca343f2b77b32ee5e23ba01fd50b1a796d3b
Alan Carlos <alancarlospereira.ti@gmail.com>
2023-11-09 17:17:51 -0300

    Testando tipo de dados e tipos de operadores

--- Semana1/MeuPrimeiroProjeto/Program.cs
+++ Semana1/MeuPrimeiroProjeto/Program.cs
@@ -1,7 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-
 #region Teste de tipos de Dados
     int tipoInteiro;
     double tipoDouble;
@@ -11,7 +10,16 @@ Console.WriteLine("Hello, World!");
     tipoInteiro = int.MaxValue;
     tipoLong = long.MaxValue;
 
+    tipoString = "100";
+    tipoInteiro = int.Parse(tipoString);
+
     Console.WriteLine("O maximo inteiro é " + tipoInteiro);
     Console.WriteLine("O maximo inteiro é " + tipoLong);
-    
+#endregion
+
+#region Teste de Operadores
+    tipoDouble = tipoInteiro + tipoLong;
+
+    tipoInteiro = 10 > 5 ? 1 : 0;
+    Console.WriteLine(tipoInteiro);
 #endregion
