b1d09e38052c0ebe12ffc10e5d642ea45dd89379
Alan Santos <alancarlospereira.ti@gmail.com>
2023-11-16 15:19:28 -0300

    Semana2 de dotnet

---
+++ Semana2/ExemplosAula/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+#region Exemplo 1
+
+#endregion
+
+#region Exemplo 2
+
+#endregion
+
+#region Exemplo 3
+
+#endregion
