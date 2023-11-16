cea35ab0c9965fb01cb8b624f44f52e0e5e949fb
Alan Santos <alancarlospereira.ti@gmail.com>
2023-11-16 15:37:23 -0300

    resolvendo exemplo1

--- Semana2/ExemplosAula/Program.cs
+++ Semana2/ExemplosAula/Program.cs
@@ -1,11 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
 
-#region Exemplo 1
-
+#region Exemplo 1 (Numeros divisiveis por 3 ou 4 ate 30)
+    for (int i = 0; i <= 30; i++){
+        if (i % 3 == 0 || i % 4 == 0){
+            Console.WriteLine(i);
+        }
+    }
 #endregion
 
 #region Exemplo 2
+    
 
 #endregion
 
