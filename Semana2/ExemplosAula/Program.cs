d876a3253d72f9af9686f9054a8d1111742ac522
Alan Carlos <alancarlospereira.ti@gmail.com>
2023-11-17 01:33:38 -0300

    Resolvendo exercicios da PI001 de dotnet

--- Semana2/ExemplosAula/Program.cs
+++ Semana2/ExemplosAula/Program.cs
@@ -1,6 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
-
 #region Exemplo 1 (Numeros divisiveis por 3 ou 4 ate 30)
     for (int i = 0; i <= 30; i++){
         if (i % 3 == 0 || i % 4 == 0){
@@ -11,9 +10,8 @@
 
 #region Exemplo 2
     
-
 #endregion
 
 #region Exemplo 3
-
+    
 #endregion
