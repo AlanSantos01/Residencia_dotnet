ddd88ff5a541a3ad674c47454f383362e612a2f1
Alan Carlos <alancarlospereira.ti@gmail.com>
2023-11-23 15:28:58 -0300

    Alguns exemplos de csharp

--- Semana3/ExemploAulas/Program.cs
+++ Semana3/ExemploAulas/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿#region tupla
+
+    var pessoa1 = CriarTupla("Alice", 25);
+    var pessoa2 = CriarTupla("Bob", 30);
+
+    ExibirResultado(pessoa1);
+    ExibirResultado(pessoa2);
+
+    static (string Nome, int Idade) CriarTupla(string nome, int idade)
+    {
+        // Retorna uma tupla contendo o nome e a idade
+        return (nome, idade);
+    }
+
+    static void ExibirResultado((string Nome, int Idade) pessoa)
+    {
+        // Exibe os resultados
+        Console.WriteLine($"Nome: {pessoa.Nome}, Idade: {pessoa.Idade}");
+    }
+#endregion
