b1a3d0fab96dca5996776f0eb5fb2e9c260fea08
Alan Carlos <alancarlospereira.ti@gmail.com>
2023-11-30 15:41:46 -0300

    Criando classe Pessoa e Advogado, ambos herdando da classe Pessoa

--- Semana4/prova-alan/Program.cs
+++ Semana4/prova-alan/Program.cs
@@ -3,3 +3,11 @@
     public string dataNascimento;
     public string cpf;
 }
+
+class Advogado : Pessoa{
+    public string CNA;
+}
+class Cliente : Pessoa{
+    public string estadoCivil;
+    public string profissao;
+}
